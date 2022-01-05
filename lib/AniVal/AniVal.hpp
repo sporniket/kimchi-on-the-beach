f64ca84a594b273f1190edb426f0f530f2a1addb
David SPORN <sporniket.studio@gmail.com>
2022-01-05 08:28:14 +0100

    add gpl notice in relevant sources

--- lib/AniVal/AniVal.hpp
+++ lib/AniVal/AniVal.hpp
@@ -1,4 +1,20 @@
-// (c) 2021 David SPORN
+// Copyright 2021,2022 David SPORN
+// ---
+// This file is part of 'Kimchi on the beach'.
+// ---
+// 'Kimchi on the beach' is free software: you can redistribute it and/or 
+// modify it under the terms of the GNU General Public License as published 
+// by the Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+
+// 'Kimchi on the beach' is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
+// Public License for more details.
+
+// You should have received a copy of the GNU General Public License along 
+// with 'Kimchi on the beach'. If not, see <https://www.gnu.org/licenses/>. 
+
 // interfaces
 // Animated values library
 // An animated value is a collection of values that can be iterated upon.
--- lib/mcual/GeneralPurposeInputOutput.hpp
+++ lib/mcual/GeneralPurposeInputOutput.hpp
@@ -1,3 +1,20 @@
+// Copyright 2021,2022 David SPORN
+// ---
+// This file is part of 'Kimchi on the beach'.
+// ---
+// 'Kimchi on the beach' is free software: you can redistribute it and/or 
+// modify it under the terms of the GNU General Public License as published 
+// by the Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+
+// 'Kimchi on the beach' is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
+// Public License for more details.
+
+// You should have received a copy of the GNU General Public License along 
+// with 'Kimchi on the beach'. If not, see <https://www.gnu.org/licenses/>. 
+
 #ifndef GENERAL_PURPOSE_INPUT_OUTPUT_HPP
 #define GENERAL_PURPOSE_INPUT_OUTPUT_HPP
 
